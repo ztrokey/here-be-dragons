27ee9a700ad41b43ef943822556ed67785fdbb4b
Zach T <ztrokey@gmail.com>
2021-06-26 15:52:58 -0600

    completed

--- test/nytimes_test.rb
+++ test/nytimes_test.rb
@@ -11,21 +11,27 @@ class NytimesTest < Minitest::Test
   end
 
   def test_it_can_get_copyright
-    #Set your code to the local variable, "result"
+    result = @hash[:copyright]
 
     assert result, "Copyright (c) 2018 The New York Times Company. All Rights Reserved."
   end
 
   def test_it_can_get_array_of_stories
-    #Set your code to the local variable, "result"
+    result = @hash[:results]
 
     assert result.is_a? (Array)
     assert_equal 44, result.count
   end
 
   def test_it_can_get_all_stories_with_subsection_of_politics
-    #Set your code to the local variable, "result"
-
+    result = []
+    @hash[:results].each do |r|
+      if
+        r[:subsection] == "Politics"
+        result << r
+      end
+      result
+    end
 
     assert result.is_a? (Array)
     assert_equal 6, result.count
@@ -34,5 +40,4 @@ class NytimesTest < Minitest::Test
     assert_equal "Conspiracy Theories Made Alex Jones Very Rich. They May Bring Him Down.",
       result.last[:title]
   end
-
 end
